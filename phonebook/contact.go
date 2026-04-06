6a9d8dcbe95edfe4627340e611ff8eb70d389beb
Ganesh Mhetre <mhetreg458@gmail.com>
2026-04-06 06:53:06 +0000

    add: UpdatePhone() and AddGroup() methods with pointer receiver

--- phonebook/contact.go
+++ phonebook/contact.go
@@ -6,3 +6,15 @@ type Contact struct {
 	Email string
 	Groups []string //slice -muliple group mein ho sakta hai
 }
+func (c *Contact) UpdatePhone(newPhone string){
+	c.Phone = newPhone
+}
+func (c *Contact) AddGroup(group string){
+
+	for _,g := range c.Groups{
+		if g == group{
+			return
+		}
+	}
+	c.Groups = append(c.Groups, group)
+}
